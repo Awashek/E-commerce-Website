c312e65e78775be92f526e89dd7ef4fcd990e2d4
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-05 23:29:42 +0545

    commit

--- src/components/Sidebar/Category/Category.jsx
+++ src/components/Sidebar/Category/Category.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 export default function Category() {
   return (
-    <div>
-      Category
+    <div className='items-start bg-red-200 mr-20'>
+      <h2>Category</h2>
+      <div className='flex flex-col pt-3'>
+        <label htmlFor="">
+          <input type="radio" name="test"/>
+          <span>All</span>
+        </label>
+
+        <label htmlFor="">
+          <input type="radio" name="test"/>
+          <span>Sneaker</span>
+        </label>
+
+        <label htmlFor="">
+          <input type="radio" name="test"/>
+          <span>Flats</span>
+        </label>
+
+        <label htmlFor="">
+          <input type="radio" name="test"/>
+          <span>Sandals</span>
+        </label>
+
+        <label htmlFor="">
+          <input type="radio" name="test"/>
+          <span>Heels</span>
+        </label>
+      </div>
     </div>
   )
 }
