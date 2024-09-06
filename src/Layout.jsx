2bed1dc6815cbc172a99b57ecc2825a22da74534
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-06 22:54:44 +0545

    commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from './components/Navigation/NavBar'
 import Footer from './components/Footer'
+import { products } from './db/Products'
+
 export default function Layout() {
+  
+  //Input filter
+  const [query, setQuery] = useState("")
+
+  const handleInputChange = (e) => {
+  setSearch(e.target.value)
+  }
+
+  const filteredItems = products.filter(product => 
+  product.title.toLowerCase().indexOf(search.toLowerCase() !== -1))
   return (
     <div>
-      <NavBar />
+      <NavBar query={query} handleInputChange={handleInputChange}/>
       <Outlet />
       <Footer />
     </div>
