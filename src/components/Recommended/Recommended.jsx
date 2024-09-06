0ba60376a3e921fe2a2551395a6377250122a652
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-07 00:11:19 +0545

    commit

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
 export default function Recommended() {
   return (
-    <div className='mt-6 mb-6 flex-grow border-b-2 border-solid 
+    <div className='mt-6 flex-grow border-b-2 border-solid 
      border-[#e5e5e5]'>
-      <h2 className='text-3xl text-gray-600 text-center pb-2'>Recommended</h2>
+      <h2 className='text-3xl text-gray-500 text-center pb-2'>Recommended</h2>
       <div className='flex pl-[32%] mb-6'>
       <button className='btns'>All Products</button>
       <button className='btns'>Nike</button>
