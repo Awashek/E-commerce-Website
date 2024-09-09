73de5d3e6cce211030ec34b148c1901027117547
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-09 22:21:15 +0545

    commit

--- src/components/Products/ProductDetails.jsx
+++ src/components/Products/ProductDetails.jsx
@@ -18,80 +18,55 @@ export default function ProductDetail() {
   }
 
   return (
-//     <div className="m-4 p-4 bg-white shadow-lg rounded-lg min-h-screen">
-//       <div className="flex flex-col items-center">
-//       <div className="mb-4">
-//         <img 
-//           src={product.img} 
-//           alt="Main" 
-//           className="w-80 h-80 object-contain rounded-lg shadow-md" 
-//         />
-//       </div>
 
-//       {/* Small Images */}
-//       <div className="flex space-x-4">
-//         {images.map((img, index) => (
-//           <img 
-//             key={index} 
-//             src={img} 
-//             alt={`Thumbnail ${index}`} 
-//             className="w-20 h-20 object-cover cursor-pointer rounded-md hover:opacity-80 transition" 
-//             onClick={() => handleImageClick(img)}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//       </div>
-//   );
-// }
-<>
-<h2 className="text-2xl font-semibold pl-5 pt-3">{product.title}</h2>
-<div className="flex justify-between gap-4 min-h-screen p-5">
-{/* Left content */}
+  <>
+    <h2 className="text-3xl font-semibold text-center pl-5 pt-3">{product.title}</h2>
+    <div className="flex justify-between gap-4 min-h-screen p-5">
+    {/* Left content */}
 
-<div className="flex flex-col gap-4 w-1/2">
-  
-  <div className='flex flex-col gap-4'>
-    <div className="w-full bg-yellow-200 pl-[13%]">
-      <img src={mainImage} alt={product.title} className="w-[400px] h-[400px] object-contain mb-5 items-center" />
+    <div className="flex flex-col gap-4 w-1/2  ">
+      
+      <div className='flex flex-col '>
+        <div className="w-full pl-[20%] border border-gray-300">
+          <img src={mainImage} alt={product.title} className="w-[400px] h-[400px] object-contain mb-5 items-center" />
+        </div>
+
+        <div className="w-full flex gap-4 flex-row pl-[25%] pt-3">
+          <img 
+            src={product.img} 
+            alt="Small 1" 
+            onClick={() => handleImageClick(product.img)} 
+            className="w-[100px] h-[100px] object-contain cursor-pointer border border-gray-200" 
+          />
+          <img 
+            src={product.img1} 
+            alt="Small 2" 
+            onClick={() => handleImageClick(product.img1)} 
+            className="w-[100px] h-[100px] object-contain cursor-pointer border border-gray-200" 
+          />
+          <img 
+            src={product.img2} 
+            alt="Small 3" 
+            onClick={() => handleImageClick(product.img2)} 
+            className="w-[100px] h-[100px] object-contain cursor-pointer border border-gray-200" 
+          />
+        </div>
+
+        <div>
+          <h2 className='text-xl text-bold text-center text-gray-700 pt-6'>Product Description</h2>
+          <p className="w-full text-gray-500 p-4">{product.description}</p>
+        </div>
+      </div>
     </div>
 
-    <div className="w-full flex gap-4 flex-row bg-red-300 pl-[13%] ">
-      <img 
-        src={product.img} 
-        alt="Small 1" 
-        onClick={() => handleImageClick(product.img)} 
-        className="w-[100px] h-[100px] object-contain cursor-pointer" 
-      />
-      <img 
-        src={product.img1} 
-        alt="Small 2" 
-        onClick={() => handleImageClick(product.img1)} 
-        className="w-[100px] h-[100px] object-contain cursor-pointer" 
-      />
-      <img 
-        src={product.img2} 
-        alt="Small 3" 
-        onClick={() => handleImageClick(product.img2)} 
-        className="w-[100px] h-[100px] object-contain cursor-pointer" 
-      />
+    {/* Right content */}
+    <div className="w-1/2 flex bg-red-200">
+      <div className=" bg-blue-100">
+        hello
+      </div>
     </div>
 
-    <div>
-      <h2 className='text-xl text-bold text-gray-700 '>Product Description</h2>
-      <p className="w-full text-gray-500">{product.description}</p>
     </div>
-  </div>
-</div>
-
-{/* Right content */}
-<div className="w-1/2 flex bg-red-200">
-  <div className=" bg-blue-100">
-    hello
-  </div>
-</div>
-
-</div>
-</>
-  );
-}
+  </>
+    );
+  }
