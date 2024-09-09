c006b0ea5499bb4dc4760246a86e272bf1d5d928
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-09 11:33:54 +0545

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
 export default function Home() {
+  const navigate = useNavigate();
 
+  const handleNavigation = () => {
+    navigate('/Products')
+  }
   return (
     <>
       <div className='relative'>
@@ -11,14 +15,12 @@ export default function Home() {
               <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white p-4">
           <h1 className="text-3xl md:text-5xl font-bold mb-4">Welcome to LaceUp!</h1>
           <p className="mb-6 text-center">Find the best shoes for your style.</p>
-          <Link
-          to="/Product"
-          >
-          <button className="bg-red-400 hover:bg-red-500 text-white font-semibold py-2 px-4 rounded">
+          
+          <button
+          onClick={handleNavigation}
+          className="bg-red-400 hover:bg-red-500 text-white font-semibold py-2 px-4 rounded">
             Shop Now
           </button>
-          </Link>
-          
           </div>
         </div>
     </>
