4d5eb3868afc3e5090d00c35c937f5b7aa221b69
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-22 18:04:50 +0545

    commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,6 +6,7 @@ import { useOutletContext } from 'react-router-dom'
 export default function Products() {
 
     const {result, handleChange, handleClick} = useOutletContext() 
+
     
   return (
   <>
