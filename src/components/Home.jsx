8f34c284c43f11cac8e14136e0f11cf9205dc8ab
Awashek Acharya <awashek.acharya16@gmail.com>
2024-09-27 12:08:42 +0545

    home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {useNavigate } from 'react-router-dom'
+export default function Home() {
+  const navigate = useNavigate();
+  const handleNavigation = () => {
+    navigate('/products')
+  }
+  return (
+    <>
+      <div className='relative'>
+            <img className="w-full h-auto object-cover"
+            src="https://www.sneakerhype.eu/cdn/shop/products/file_fc3589a9-2e6b-4daa-9223-4a11922a4fc8.jpg?v=1710154347" alt="logo" 
+            />    
+              <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white p-4">
+          <h1 className="text-3xl md:text-5xl font-bold mb-4">Welcome to LaceUp!</h1>
+          <p className="mb-6 text-center">Find the best shoes for your style.</p>
+          
+          <button
+          onClick={handleNavigation}
+          className="bg-red-400 hover:bg-red-500 text-white font-semibold py-2 px-4 rounded">
+            Shop Now
+          </button>
+          </div>
+        </div>
+    </>
+  )
+}
